3d47828f754ed66098c7ca167a56119459c34b7f
CODE_WRANGLER <jack@bonwick.dev>
2020-11-29 20:18:32 +1100

    Created console header and source file

---
+++ Adept-CIM-centre-Controller/Adept-CIM-centre-Controller-Core/src/console.c
@@ -0,0 +1,6 @@
+/*
+ * console.c
+ *
+ * Created: 29/11/2020 8:17:39 PM
+ *  Author: jbonwick
+ */ 
---
+++ Adept-CIM-centre-Controller/Adept-CIM-centre-Controller-Core/src/console.h
@@ -0,0 +1,16 @@
+/*
+ * console.h
+ *
+ * Created: 29/11/2020 8:17:27 PM
+ *  Author: jbonwick
+ */ 
+
+
+#ifndef CONSOLE_H_
+#define CONSOLE_H_
+
+
+
+
+
+#endif /* CONSOLE_H_ */
