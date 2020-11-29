8c10987942d72e8673c4e62976a9b0657fb53d85
CODE_WRANGLER <jack@bonwick.dev>
2020-11-29 19:13:46 +1100

    Formatted document

--- Adept-CIM-centre-Controller/Adept-CIM-centre-Controller-Core/src/main.c
+++ Adept-CIM-centre-Controller/Adept-CIM-centre-Controller-Core/src/main.c
@@ -1,33 +1,33 @@
 /**
- * \file
- *
- * \brief Empty user application template
- *
- */
+* \file
+*
+* \brief Empty user application template
+*
+*/
 
 /**
- * \mainpage User Application template doxygen documentation
- *
- * \par Empty user application template
- *
- * Bare minimum empty user application template
- *
- * \par Content
- *
- * -# Include the ASF header files (through asf.h)
- * -# "Insert system clock initialization code here" comment
- * -# Minimal main function that starts with a call to board_init()
- * -# "Insert application code here" comment
- *
- */
+* \mainpage User Application template doxygen documentation
+*
+* \par Empty user application template
+*
+* Bare minimum empty user application template
+*
+* \par Content
+*
+* -# Include the ASF header files (through asf.h)
+* -# "Insert system clock initialization code here" comment
+* -# Minimal main function that starts with a call to board_init()
+* -# "Insert application code here" comment
+*
+*/
 
 /*
- * Include header files for all drivers that have been imported from
- * Atmel Software Framework (ASF).
- */
+* Include header files for all drivers that have been imported from
+* Atmel Software Framework (ASF).
+*/
 /*
- * Support and FAQ: visit <a href="https://www.microchip.com/support/">Microchip Support</a>
- */
+* Support and FAQ: visit <a href="https://www.microchip.com/support/">Microchip Support</a>
+*/
 #include <asf.h>
 #include "cim_centre_io.h"
 
@@ -39,5 +39,6 @@ int main (void)
 	//Initialize io pins on the bus
 	io_init();
 
-	/* Insert application code here, after the board has been initialized. */
+	while(1){
+	}
 }
