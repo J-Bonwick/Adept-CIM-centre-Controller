16b7ef6db78030a0cf7275760eb402ae5fae8a92
CODE_WRANGLER <jack@bonwick.dev>
2020-11-30 20:36:26 +1100

    Commented out test code

--- Adept-CIM-centre-Controller/Adept-CIM-centre-Controller-Core/src/main.c
+++ Adept-CIM-centre-Controller/Adept-CIM-centre-Controller-Core/src/main.c
@@ -43,7 +43,7 @@ int main (void)
 	io_init();
 
 	while(1){
-		usart_write_line(CONF_UART, "TEST\r\n");
+		//usart_write_line(CONF_UART, "TEST\r\n");
 		//usart_putchar(CONF_UART, 'b');
 		//uint32_t dw_status = usart_get_status(CONF_UART);
 		//if (dw_status & US_CSR_RXRDY) {
