e7ad2f699ddec7b4963028073b1cddc7e2392ab7
Jack <jack@bonwick.dev>
2020-12-28 15:22:22 +1100

    Commented out unused code

--- Adept-CIM-centre-Controller/Adept-CIM-centre-Controller-Core/src/main.c
+++ Adept-CIM-centre-Controller/Adept-CIM-centre-Controller-Core/src/main.c
@@ -36,7 +36,7 @@
 #include "console.h"
 #include "pid.h"
 
-void updateEncoder(long int *pos, uint16_t encoderCounter);
+void updateEncoder(int *pos, uint16_t encoderCounter);
 
 void configure_rtt(void){
 	rtt_init(RTT, 30);
@@ -54,13 +54,13 @@ int main (void)
 	io_init();
 	
 	//Set axis positions to the current pos so that the rollover logic doesn't trigger a roll over
-	long int fb_current = readEncoder(encoderFB);
-	long int lr_current = readEncoder(encoderLR);
-	long int ud_curent = readEncoder(encoderUD);
+	int fb_current = readEncoder(encoderFB);
+	int lr_current = readEncoder(encoderLR);
+	int ud_curent = readEncoder(encoderUD);
 	//Set target positions to the current position
-	long int fb_target = fb_current;
-	long int lr_target = lr_current;
-	long int ud_target = ud_curent;
+	int fb_target = fb_current;
+	int lr_target = lr_current;
+	int ud_target = ud_curent;
 	//Current Axis speed
 	int fb_speed = 0;
 	int lr_speed = 0;
@@ -73,8 +73,8 @@ int main (void)
 	bool LRhoming = false;
 	
 	//PID structs
-	pid_t fb_pid;
-	pid_t lr_pid;
+	struct pid_t fb_pid;
+	struct pid_t lr_pid;
 	//Configure the PID structs
 	pidConfigure(&fb_pid, &fb_current, &fb_speed, &fb_target, 1, 0, 0, -2024, 2024);
 	pidConfigure(&lr_pid, &lr_current, &lr_speed, &lr_target, 0.5, 0, 0, -2024, 2024);
@@ -94,8 +94,8 @@ int main (void)
 		//Calculate PID if alarm has occurred in the RTT
 		if((rtt_get_status(RTT) & RTT_SR_ALMS) == RTT_SR_ALMS){
 			//Get out put values for setting the DACs
-			pidCalculate(fb_pid);
-			pidCalculate(lr_pid);
+			pidCalculate(&fb_pid);
+			pidCalculate(&lr_pid);
 			
 			//Add 2047 to center the values around 2048 as zero speed
 			setDacs(fb_speed + 2047, lr_speed + 2047, 2048, 2048);
@@ -127,101 +127,90 @@ int main (void)
 		generalIO = readIO();
 		
 		
-		//home logic
-		if(FBhoming){
-			if(generalIO & FB_ENDSTOP){
-				FBhoming = false;
-				//resetEncoder(encoderResetFB);
-			}
-			fb_target -= 40;
-		}
-		if(LRhoming){
-			if(generalIO & LR_ENDSTOP){
-				LRhoming = false;
-				//resetEncoder(encoderResetLR);
-			}
-			lr_target += 40;
-		}
-		
-		if(frontPanelStatus & FP_HOME){
-			if(frontPanelStatus & FP_Z_PLUS){
-				LRhoming = true;
-			}
-			if(frontPanelStatus & FP_X_PLUS){
-				FBhoming = true;
-			}
-		}
-		
-		//switch (frontPanelStatus){
-			//case FP_X_PLUS:
-				//fb_target -= 40;
-				//break;
-			//case FP_X_MINUS:
-				//fb_target += 40;
-				//break;
-			//case FP_Z_PLUS:
-				//lr_target -= 40;
-				//break;
-			//case FP_Z_MINUS:
-				//lr_target += 40;
-			//default:
-				//
-			//
+		////home logic
+		//if(FBhoming){
+			//if(generalIO & FB_ENDSTOP){
+				//FBhoming = false;
+				////resetEncoder(encoderResetFB);
+			//}
+			//fb_target -= 40;
+		//}
+		//if(LRhoming){
+			//if(generalIO & LR_ENDSTOP){
+				//LRhoming = false;
+				////resetEncoder(encoderResetLR);
+			//}
+			//lr_target += 40;
+		//}
+		//
+		//if(frontPanelStatus & FP_HOME){
+			//if(frontPanelStatus & FP_Z_PLUS){
+				//LRhoming = true;
+			//}
+			//if(frontPanelStatus & FP_X_PLUS){
+				//FBhoming = true;
+			//}
 		//}
 		
-		if(frontPanelStatus & FP_X_PLUS){
-			setDacs(4095, 2048, 2045, 2048);
-		}
-		else if(frontPanelStatus & FP_X_MINUS){
-			setDacs(0, 2048, 2045, 2048);
-		}
-		else if(frontPanelStatus & FP_Z_MINUS){
-			setDacs(2042, 2048, 4095, 2048);
-		}
-		else if(frontPanelStatus & FP_Z_PLUS){
-			setDacs(2042, 2048, 0, 2048);
-		}
-		else {
-			setDacs(2042, 2048, 2045, 2048);
-		}
+		//if(frontPanelStatus & FP_X_PLUS){
+			//setDacs(4095, 2048, 2045, 2048);
+		//}
+		//else if(frontPanelStatus & FP_X_MINUS){
+			//setDacs(0, 2048, 2045, 2048);
+		//}
+		//else if(frontPanelStatus & FP_Z_MINUS){
+			//setDacs(2042, 2048, 4095, 2048);
+		//}
+		//else if(frontPanelStatus & FP_Z_PLUS){
+			//setDacs(2042, 2048, 0, 2048);
+		//}
+		//else {
+			//setDacs(2042, 2048, 2045, 2048);
+		//}
 		if(frontPanelStatus & FP_Z_PLUS){
 			lr_target += 40;
 		}
 		if(frontPanelStatus & FP_Z_MINUS){
 			lr_target -= 40;
 		}
+		if(frontPanelStatus & FP_X_PLUS){
+			fb_target += 40;
+		}
+		if(frontPanelStatus & FP_X_MINUS){
+			fb_target -= 40;
+		}
 		
 		
 		if((rtt_get_status(RTT) & RTT_SR_ALMS) == RTT_SR_ALMS){
 		//Print some useful info to the console
-		char consoleBuffer[1000] = "HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH";
+		char consoleBuffer[100];
+		//usart_write_line(CONF_UART, consoleBuffer);
+		sprintf(consoleBuffer, "FB: %d\r\n", fb_current);
 		usart_write_line(CONF_UART, consoleBuffer);
-		//sprintf(consoleBuffer, "FB: %ld\r\n", fb_pos);
-		//usart_write_line(CONF_UART, consoleBuffer);
-		//sprintf(consoleBuffer, "FBT: %ld\r\n", fb_target);
-		//usart_write_line(CONF_UART, consoleBuffer);
-		//sprintf(consoleBuffer, "LR: %ld\r\n", lr_pos);
-		//usart_write_line(CONF_UART, consoleBuffer);
-		//sprintf(consoleBuffer, "LRT: %ld\r\n", lr_target);
-		//usart_write_line(CONF_UART, consoleBuffer);
-		//sprintf(consoleBuffer, "UD: %ld\r\n", ud_pos);
-		//usart_write_line(CONF_UART, consoleBuffer);
-		//sprintf(consoleBuffer, "S: %d\r\n", readEncoder(encoderSpindle));
-		//usart_write_line(CONF_UART, consoleBuffer);
-		//sprintf(consoleBuffer, "FP: %d\r\n", readFrontPanel());
-		//usart_write_line(CONF_UART, consoleBuffer);
-		//sprintf(consoleBuffer, "P0: %d\r\n", readIO());
-		//usart_write_line(CONF_UART, consoleBuffer);
+		sprintf(consoleBuffer, "FBT: %d\r\n", fb_target);
+		usart_write_line(CONF_UART, consoleBuffer);
+		sprintf(consoleBuffer, "LR: %d\r\n", lr_current);
+		usart_write_line(CONF_UART, consoleBuffer);
+		sprintf(consoleBuffer, "LRT: %d\r\n", lr_target);
+		usart_write_line(CONF_UART, consoleBuffer);
+		sprintf(consoleBuffer, "UD: %d\r\n", ud_curent);
+		usart_write_line(CONF_UART, consoleBuffer);
+		sprintf(consoleBuffer, "S: %d\r\n", readEncoder(encoderSpindle));
+		usart_write_line(CONF_UART, consoleBuffer);
+		sprintf(consoleBuffer, "FP: %d\r\n", readFrontPanel());
+		usart_write_line(CONF_UART, consoleBuffer);
+		sprintf(consoleBuffer, "P0: %d\r\n", readIO());
+		usart_write_line(CONF_UART, consoleBuffer);
 		configure_rtt();
 		}
 	}
 }
 
-void updateEncoder(long int *pos, uint16_t encoderCounter){
+void updateEncoder(int *pos, uint16_t encoderCounter){
 	//Check for rollover
 	if(abs((*pos & 0xFFFF) - encoderCounter) > 30000){
 		//Check if roll down
-		long int temp = *pos >> 16;
+		int temp = *pos >> 16;
 		if(encoderCounter & 0x8000){
 			temp--;
 		}
